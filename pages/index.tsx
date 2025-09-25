81f4fd6ecf3228b4087dae2033a2f7e47305e565
Raymond Mawina <mawina.projects@gmail.com>
2025-09-25 12:45:09 +0200

    undo experiment

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home() {
           />
         </div>
 
-        <section className="grid justify-self-center text-left space-y-2 w-8/11 lg:w-2/3 p-1 border-2 border-green-400/40 rounded-md">
+        <section className="grid justify-self-center text-left space-y-2 w-9/11 lg:w-2/3 p-1 border-2 border-green-400/40 rounded-md">
           <h1 className="text-center font-bold">
             Raymond Mawina / LeGlaringWizard
           </h1>
@@ -61,7 +61,7 @@ export default function Home() {
         </section>
       </header>
       <main>
-        <section className="flex flex-col justify-self-center text-left space-y-2 w-full lg:w-2/3 p-1 px-2 border-2 border-yellow-500/60 rounded-md min-h-80 lg:min-h-100 lag:max-h-dvh">
+        <section className="flex flex-col justify-self-center text-left space-y-2 w-9/11 lg:w-2/3 p-1 px-2 border-2 border-yellow-500/60 rounded-md min-h-80 lg:min-h-100 lag:max-h-dvh">
           <nav>
             <ul className="flex flex-wrap justify-between space-x-2 lg:space-x-2 w-full">
               {buttons.map((item) => (
@@ -149,7 +149,7 @@ export default function Home() {
         </section>
       </main>
       <footer>
-        <section className="flex flex-col justify-self-center text-center space-y-2 w-full lg:w-2/3 p-2 border-2 border-green-400/40 rounded-md">
+        <section className="flex flex-col justify-self-center text-center space-y-2 w-9/11 lg:w-2/3 p-2 border-2 border-green-400/40 rounded-md">
           <p>🪄 Forged in code by LeGlaringWizard · 2025</p>
         </section>
       </footer>
