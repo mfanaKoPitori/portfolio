e8b113891cedabbb2a6010103d4842ed364536cd
Raymond Mawina <mawina.projects@gmail.com>
2025-11-23 20:46:33 +0200

    fix reduced the text size in the footer

--- pages/components/Footer/Presentation.tsx
+++ pages/components/Footer/Presentation.tsx
@@ -10,7 +10,7 @@ const Presentation: React.FC<FooterInfoProps> = ({ nickname }) => {
     <>
       <footer className="w-full">
         <section className="flex flex-col justify-center items-center text-center space-y-1 w-full max-w-5xl p-1">
-          <p className="text-gray-700 text-sm sm:text-base">
+          <p className="text-gray-700 text-sm lg:text-md">
             🪄 Forged in code by{" "}
             <span className="font-semibold text-amber-500">{nickname}</span> ·
             2025
