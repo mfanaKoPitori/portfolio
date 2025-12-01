524e15ec1b8032a61d25d609d8d6ae64934d4cc4
Raymond Mawina <mawina.projects@gmail.com>
2025-12-01 11:43:43 +0200

    fix: made the code DRY in the about page by moving state from presentation

--- components/About/Index.tsx
+++ components/About/Index.tsx
@@ -5,17 +5,75 @@ import Presentation from "./Presentation";
 interface AboutProps {
   name: string;
 }
+
+interface EducationInstitutionProp {
+  school_name: string;
+  course: string;
+  period: string;
+  completion_status: boolean;
+}
+
+interface WorkExperienceProp {
+  role: string;
+  company_name: string;
+  period: string;
+}
+
 interface AboutPageProps {
 }
 
 
 const About: React.FC<AboutPageProps> = () => {
-  const [aboutInfo, _] = useState<AboutProps>(
+  const [educationInfo, _] = useState<EducationInstitutionProp[]>([
     {
-      name: "Raymond Mawina",
+      school_name: "Shirinda Primary School",
+      course: "Mathematics and Science",
+      period: "2004 - 2012",
+      completion_status: true
+    },
+    {
+      school_name: "Jafta Mahlangu High School",
+      course: "Mathematics and Science",
+      period: "2012 - 2016",
+      completion_status: true
+    },
+    {
+      school_name: "University of Pretoria",
+      course: "Bachelors in Information Technology",
+      period: "2018 - 2020",
+      completion_status: false
+    },
+    {
+      school_name: "Umuzi Photo Club",
+      course: "NCIT in Information Technology",
+      period: "2022 - 2023",
+      completion_status: true
     }
-  );
-  return <Presentation name={aboutInfo.name}/>;
+  ]);
+
+    const [workExperienceInfo, setWorkExperienceInfo] = useState<WorkExperienceProp[]>([
+      {
+        role: "Software Developer Apprentice",
+        company_name: "Umuzi Photo Club",
+        period: "2022 - 2023",
+      },
+      {
+        role: "Software Developer Intern",
+        company_name: "Umuzi Photo Club",
+        period: "2023 - 2023",
+      },
+      {
+        role: "Junior Software Developer",
+        company_name: "Umuzi Photo Club",
+        period: "2023 - 2024",
+      },    
+      {
+        role: "Intermediate Software Developer",
+        company_name: "Umbono Cloud Services",
+        period: "2024 - Present",
+      }
+  ]);
+  return <Presentation educationInfo={educationInfo} workExperienceInfo={workExperienceInfo}/>;
 };
 
 export default About;
--- components/About/Presentation.tsx
+++ components/About/Presentation.tsx
@@ -1,11 +1,25 @@
 import Image from "next/image";
 import React from "react";
 
-interface AboutProps {
-  name: string;
+interface EducationInstitutionProp {
+  school_name: string;
+  course: string;
+  period: string;
+  completion_status: boolean;
 }
 
-const Presentation: React.FC<AboutProps> = ({ name }) => {
+interface WorkExperienceProp {
+  role: string;
+  company_name: string;
+  period: string;
+}
+
+interface AboutProps {
+  educationInfo: EducationInstitutionProp[];
+  workExperienceInfo: WorkExperienceProp[];
+}
+
+const Presentation: React.FC<AboutProps> = ({ educationInfo, workExperienceInfo }) => {
   return (
     <>
       <section className="flex flex-col w-full md:w-full lg:7/11 xl:w-8/11 space-y-2 text-xs text-gray-600">
@@ -49,103 +63,38 @@ const Presentation: React.FC<AboutProps> = ({ name }) => {
         <section className="bg-white/80 rounded-md border border-gray-200 py-3 px-6 text-gray-600">
           <h1 className="font-bold">Educational History</h1>
           <section className="columns-1 md:columns-2 px-2 my-2 space-y-2 w-full md:gap-2">
-            <section className="bg-gray-400/10 p-2 rounded-md columns-2 w-full flex justify-between">
-                <section>
-                  <p className="font-semibold">Mathematics and Science</p>
-                  <p>Primary School</p>
-                </section>
+            {educationInfo.map((education) => 
+              (<section className="bg-gray-400/10 p-2 rounded-md columns-1 sm:columns-2 lg:columns-2 w-full grid sm:flex sm:justify-between">
+                  <section className="w-full sm:w-2/3 lg:w-3/4">
+                    <p className="font-semibold">{education.course}</p>
+                    <p>{education.school_name}</p>
+                  </section>
 
-                <section className="w-1/3 lg:w-1/4 grid justify-end">
-                    <p className="bg-green-400/10 px-1 w-fit rounded-md text-green-600">Completed</p>
-                    <p className="px-1">2005 - 2011</p>
-                </section>
-            </section>
-
-            <section className="bg-gray-400/10 p-2 rounded-md sm:columns-1 columns-2 lg:columns-2 w-full grid md:flex justify-between">
-                <section>
-                  <p className="font-semibold">Mathematics and Science</p>
-                  <p>High School</p>
-                </section>
-
-                <section className="w-full md:w-1/3 lg:w-1/4 grid md:justify-end">
-                    <p className="bg-green-400/10 w-fit h-fit rounded-md text-green-600">Completed</p>
-                    <p className="">2012 - 2016</p>   
-                </section>
-            </section>
-
-            <section className="bg-gray-400/10 p-2 rounded-md columns-2 lg:columns-2 w-full flex justify-between">
-                <section>
-                  <p className="font-semibold">Bachelors in IT</p>
-                  <p>University of Pretoria</p>
-                </section>
-
-                <section className="w-1/3 lg:w-1/4 grid justify-end">
-                    <p className="bg-red-400/10 px-1 w-fit h-fit rounded-md text-red-600">Incomplete</p>
-                    <p className="px-1">2018 - 2020</p>
-                </section>
-            </section>
-
-            <section className="bg-gray-400/10 p-2 rounded-md columns-2 lg:columns-2 w-full flex justify-between">
-                <section>
-                  <p className="font-semibold">NCIT In Information Technology</p>
-                  <p>Umuzi Photo Club</p>
-               
-                </section>
-
-                <section className="w-1/3 lg:w-1/4 grid justify-end">
-                    <p className="bg-green-400/10 px-1 w-fit h-fit rounded-md text-green-600">Completed</p>
-                    <p className="px-1">2022 - 2023</p>
-                </section>
-            </section>
+                  <section className="w-full sm:w-1/3 lg:w-1/4 grid sm:justify-end">
+                      <p className={`w-fit px-2 rounded-full ${education.completion_status ? "bg-green-700/10 text-green-600":"bg-red-700/10 text-red-600"}`}>{education.completion_status ? "Completed": "Incompleted"}</p>
+                      <p className="px-2">{education.period}</p>
+                  </section>
+              </section>
+              )
+            )}
           </section>
         </section>
 
         <section className="bg-white/80 rounded-md border border-gray-200 py-3 px-6 text-gray-600">
           <h1 className="font-bold">Work Experience</h1>
           <section className="columns-1 md:columns-2 px-2 my-2 space-y-2 w-full md:gap-2">
-            <section className="bg-gray-400/10 p-2 rounded-md columns-2 w-full flex justify-between">
-                <section className="w-full md:w-3/4">
-                  <p className="font-semibold">Software Developer Apprentice</p>
-                  <p>Umuzi Photo Club</p>
-                </section>
+            {workExperienceInfo.map((workExperience) => 
+              (<section className="bg-gray-400/10 p-2 rounded-md columns-1 sm:columns-2 lg:columns-2 w-full grid sm:flex sm:justify-between">
+                  <section className="w-full sm:w-2/3 lg:w-3/4">
+                    <p className="font-semibold">{workExperience.role}</p>
+                    <p>{workExperience.company_name}</p>
+                  </section>
 
-                <section className="w-full md:w-1/4 grid justify-end">
-                    <p>2022 - 2023</p>
-                </section>
-            </section>
-
-            <section className="bg-gray-400/10 p-2 rounded-md columns-2 lg:columns-2 w-full flex justify-between">
-                <section className="w-full md:w-3/4">
-                  <p className="font-semibold">Software Developer Intern</p>
-                  <p>Umuzi Photo Club</p>
-                </section>
-
-                <section className="w-full md:w-1/4 grid justify-end">
-                    <p>2023 - 2023</p>
-                </section>
-            </section>
-
-            <section className="bg-gray-400/10 p-2 rounded-md columns-2 lg:columns-2 w-full flex justify-between">
-                <section className="w-full md:w-3/4">
-                  <p className="font-semibold">Junior Software Developer</p>
-                  <p>Umuzi Photo Club</p>
-                </section>
-
-                <section className="w-full md:w-1/4 grid justify-end">
-                    <p>2023 - 2024</p>
-                </section>
-            </section>
-
-            <section className="bg-gray-400/10 p-2 rounded-md columns-2 lg:columns-2 w-full flex justify-between">
-                <section className="w-full md:w-3/4">
-                  <p className="font-semibold">Intermediate Software Developer</p>
-                  <p>Umbono Cloud Services</p>
-                </section>
-
-                <section className="w-full md:w-1/4 grid justify-end">
-                    <p>2024 - Present</p>
-                </section>
-            </section>
+                  <section className="w-full sm:w-1/3 lg:w-1/4 grid sm:justify-end">
+                      <p className="px-2">{workExperience.period}</p>
+                  </section>
+              </section>)
+            )}
           </section>
         </section>
 
