df4a26280404f68bd08a3de043b0501cf2cd33e3
Raymond Mawina <mawina.projects@gmail.com>
2025-12-03 21:09:53 +0200

    fix: changed header and nav bar to use pink colors

--- components/NavigationTabs/Presentation.tsx
+++ components/NavigationTabs/Presentation.tsx
@@ -15,16 +15,16 @@ const Presentation: React.FC<NavigationTabsInfoProps> = ({
 }) => {
   return (
     <>
-      <nav className="mb-2 flex flex-col justify-center items-center w-full">
-        <ul className="flex flex-wrap gap-0.1 justify-center sm:justify-left space-x-2 w-7/11 py-1 px-2">
+      <nav className="mb-2 flex flex-col items-center w-10/11 lg:w-8/11 xl:w-6/11 space-y-2 text-xs text-gray-600">
+        <ul className="flex flex-wrap gap-0.1 justify-between space-x-2 w-7/11 py-1 px-2">
           {buttonList?.map((item) => (
             <li key={item}>
               <Link
                 href={item === "About" ? "/":`/${item.toLocaleLowerCase()}`}
                 className={`text-sm lg:text-md ${
                   activeButton === item
-                    ? "green-amber-500 text-green-600"
-                    : " text-gray-600 hover:text-green-800"
+                    ? "green-amber-500 text-pink-600/80"
+                    : " text-gray-600 hover:text-pink-400/60"
                 }`}
                 onClick={() => {
                   if (activeButton !== item) handleButtonClick(item);
