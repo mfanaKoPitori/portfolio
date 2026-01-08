7795afacec0b1fbbbb847bc714ab65f51db16eeb
Raymond Mawina <mawina.projects@gmail.com>
2026-01-08 10:58:23 +0200

    fix: move sections around

--- components/About/Presentation.tsx
+++ components/About/Presentation.tsx
@@ -70,6 +70,22 @@ const Presentation: React.FC<AboutProps> = ({ educationInfo, workExperienceInfo,
         </section>
 
         <section className="rounded-md border border-gray-300/20 py-3 px-6 text-gray-500/90 bg-gray-100/10">
+          <h1 className="font-mono">Technical Skills</h1>
+          <section className="grid md:grid-cols-2 my-2 space-y-2">
+            {techSkillsInfo.map((techSkillCategory, index) => (
+              <section key={index} className="grid space-y-1">
+                <p className="w-fit text-gray-600">{techSkillCategory.category}</p>
+                <section className="flex flex-wrap gap-x-1 gap-y-1">
+                  {techSkillCategory.skills.map((skill, index) => (
+                    <p key={index} className="bg-gray-300/20 border-1 border-gray-300/60 rounded-full px-1 py-0.5">{skill.name}</p>  
+                  ))}
+                </section>
+              </section>    
+            ))}
+          </section>
+        </section>
+
+        <section className="rounded-md border border-gray-300/20 py-3 px-6 text-gray-500/90 bg-gray-100/10">
           <h1 className="font-mono">Projects</h1>
           <section className="columns-1 flex flex-col md:columns-2 md:flex md:flex-row gap-x-2 py-2 gap-y-2">
             <section className="rounded-md border border-gray-300/20 p-2 text-gray-500/90 bg-gray-100/10 w-full md:w-1/2">
@@ -97,22 +113,6 @@ const Presentation: React.FC<AboutProps> = ({ educationInfo, workExperienceInfo,
           <section className="flex justify-end space-x-4">
             <button>Prev</button>
             <button>Next</button>
-          </section>
-        </section>
-
-        <section className="rounded-md border border-gray-300/20 py-3 px-6 text-gray-500/90 bg-gray-100/10">
-          <h1 className="font-mono">Technical Skills</h1>
-          <section className="grid md:grid-cols-2 my-2 space-y-2">
-            {techSkillsInfo.map((techSkillCategory, index) => (
-              <section key={index} className="grid space-y-1">
-                <p className="w-fit text-gray-600">{techSkillCategory.category}</p>
-                <section className="flex flex-wrap gap-x-1 gap-y-1">
-                  {techSkillCategory.skills.map((skill, index) => (
-                    <p key={index} className="bg-gray-300/20 border-1 border-gray-300/60 rounded-full px-1 py-0.5">{skill.name}</p>  
-                  ))}
-                </section>
-              </section>    
-            ))}
           </section>
         </section>
       </section>
